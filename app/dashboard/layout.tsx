bfbe82f8b1bb4d0b2b17b81be3d33f3642157266
Marco <marcodefalco2017@libero.it>
2025-10-10 23:54:25 +0200

    deploy fix

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -38,7 +38,7 @@ export default async function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="bg-light text-dark grid min-h-screen grid-cols-1 grid-rows-[auto_1fr] text-sm md:grid-cols-[auto_1fr] md:grid-rows-[1fr] md:gap-y-4 xl:grid-cols-[var(--spacing-sidebar)_1fr] dark:bg-gradient-to-br dark:from-zinc-900 dark:to-zinc-800 dark:text-gray-100">
+    <div className="bg-light text-dark grid h-screen grid-cols-1 grid-rows-[auto_1fr] text-sm md:grid-cols-[auto_1fr] md:grid-rows-[1fr] md:gap-y-4 xl:grid-cols-[var(--spacing-sidebar)_1fr] dark:bg-gradient-to-br dark:from-zinc-900 dark:to-zinc-800 dark:text-gray-100">
       <DrawerContextProvider>
         <Drawer className="inline-flex items-start px-1 py-1.5 md:py-7 xl:hidden">
           <Logo />
@@ -64,7 +64,7 @@ export default async function Layout({
         }}
       />
 
-      <main className="_h-full overflow-y-auto border-l border-gray-200 bg-white px-(--page-padding-x) pt-5 pb-14 [--page-padding-x:--spacing(3)] sm:px-4 xl:px-10 xl:pt-16.5 xl:pb-30 dark:border-none dark:bg-zinc-900/80 dark:backdrop-blur-md">
+      <main className="h-full overflow-y-auto border-l border-gray-200 bg-white px-(--page-padding-x) pt-5 pb-14 [--page-padding-x:--spacing(3)] sm:px-4 xl:px-10 xl:pt-16.5 xl:pb-30 dark:border-none dark:bg-zinc-900/80 dark:backdrop-blur-md">
         {children}
       </main>
     </div>

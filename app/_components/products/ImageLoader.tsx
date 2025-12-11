26a9863d5a92784cff84ae02ba99fabd5054de08
Marco <marcodefalco2017@libero.it>
2025-12-11 21:37:38 +0100

    image loader refactored

--- app/_components/products/ImageLoader.tsx
+++ app/_components/products/ImageLoader.tsx
@@ -68,12 +68,13 @@ function ImageLoader({
     const files = e.dataTransfer.files;
     handleFileSelect(files);
   };
+
   return (
     <form onSubmit={handleAddProductImages}>
       <div
         onDragOver={handleDragOver}
         onDrop={handleDrop}
-        className="flex min-h-40 min-w-full justify-center rounded-lg border border-dashed border-gray-900/25 p-5 sm:w-sm dark:border-white/30"
+        className={`flex min-h-40 min-w-full justify-center rounded-lg border border-dashed border-pink-500 bg-pink-50/50 p-5 sm:w-sm dark:border-indigo-500 dark:bg-indigo-950/20 ${isSubmitting || (removedImages && removedImages.length > 0) ? "cursor-not-allowed opacity-50" : ""}`}
       >
         {selectedImages.length === 0 && (
           <div className="text-center">
@@ -82,7 +83,7 @@ function ImageLoader({
               fill="currentColor"
               data-slot="icon"
               aria-hidden="true"
-              className="mx-auto size-12 text-gray-300 dark:text-white/30"
+              className="mx-auto size-12 text-pink-700/40 dark:text-indigo-500/60"
             >
               <path
                 d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
@@ -90,7 +91,7 @@ function ImageLoader({
                 fillRule="evenodd"
               />
             </svg>
-            <div className="mt-4 flex flex-wrap justify-center text-center text-[15px] text-gray-400 sm:text-sm/6 dark:text-white/30">
+            <div className="mt-4 flex flex-wrap justify-center text-center text-[15px] text-pink-700/60 sm:text-sm/6 dark:text-indigo-500/80">
               <label
                 htmlFor="file-upload"
                 className={`touch-hitbox relative rounded-md bg-transparent font-semibold text-pink-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-pink-600 dark:text-indigo-400 dark:focus-within:outline-indigo-400 ${
@@ -123,7 +124,7 @@ function ImageLoader({
                 o trascinale e rilasciale
               </p>
             </div>
-            <p className="mt-1 text-sm text-gray-400 sm:mt-0 sm:text-xs/5 dark:text-white/30">
+            <p className="mt-1 text-sm text-pink-700/60 sm:mt-0 sm:text-xs/5 dark:text-indigo-500/80">
               PNG, JPG, GIF fino a 1MB
             </p>
           </div>
@@ -134,7 +135,7 @@ function ImageLoader({
               {selectedImages.map((image, i) => (
                 <div
                   key={image.name + i}
-                  className={`dark:text-light/80 flex items-center justify-center gap-1 rounded-lg border border-gray-200 py-0.5 pr-2 pl-1 text-neutral-700/80 dark:border-zinc-700/40 dark:hover:bg-zinc-800/90 ${isSubmitting ? "cursor-not-allowed opacity-50" : ""}`}
+                  className={`flex items-center justify-center gap-1 rounded-lg border border-pink-200 py-0.5 pr-2 pl-1 text-pink-700/80 hover:bg-pink-100/50 dark:border-indigo-700/40 dark:text-indigo-400/80 dark:hover:bg-indigo-950/60 ${isSubmitting ? "cursor-not-allowed opacity-50" : ""}`}
                 >
                   <button
                     type="button"
@@ -146,7 +147,7 @@ function ImageLoader({
                         ),
                       );
                     }}
-                    className="touch-hitbox cursor-pointer rounded-lg p-0.5 hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50 dark:hover:bg-zinc-900"
+                    className="touch-hitbox cursor-pointer rounded-lg p-0.5 hover:bg-pink-200/50 disabled:cursor-not-allowed disabled:opacity-50 dark:hover:bg-zinc-900"
                   >
                     <XMarkIcon className="size-4.5" />
                   </button>
@@ -157,15 +158,15 @@ function ImageLoader({
             <div className="flex gap-3">
               <button
                 type="submit"
-                className="touch-hitbox flex cursor-pointer items-center justify-center rounded-lg border border-inherit bg-lime-500 px-3 py-2 text-sm font-semibold text-white inset-shadow-2xs transition-colors duration-200 hover:bg-lime-600 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-700/40 dark:bg-lime-700 dark:shadow-sm dark:inset-shadow-white/20 dark:hover:bg-lime-600"
+                className="touch-hitbox flex cursor-pointer items-center justify-center rounded-lg border border-inherit bg-pink-500 px-3 py-2 text-sm font-semibold text-white inset-shadow-2xs transition-colors duration-200 hover:bg-pink-600 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-700/40 dark:bg-indigo-700 dark:shadow-sm dark:inset-shadow-white/20 dark:hover:bg-indigo-600"
                 disabled={isSubmitting}
               >
-                Carica
+                Salva
               </button>
               <button
                 type="button"
                 onClick={() => setSelectedImages([])}
-                className="touch-hitbox flex cursor-pointer items-center justify-center rounded-lg border border-inherit bg-stone-500 px-3 py-2 text-sm font-semibold text-white inset-shadow-2xs transition-colors duration-200 hover:bg-stone-600 disabled:cursor-not-allowed disabled:opacity-50 dark:border-zinc-700/40 dark:bg-stone-800 dark:shadow-sm dark:inset-shadow-white/20 dark:hover:bg-stone-900"
+                className="touch-hitbox _dark:border-zinc-700/40 flex cursor-pointer items-center justify-center rounded-lg border border-pink-100 bg-pink-100 px-3 py-2 text-sm font-semibold text-pink-500 inset-shadow-2xs transition-colors duration-200 hover:bg-pink-200 disabled:cursor-not-allowed disabled:opacity-50 dark:border-indigo-950/50 dark:bg-indigo-950/50 dark:text-indigo-500 dark:shadow-sm dark:inset-shadow-white/20 dark:hover:bg-indigo-950"
                 disabled={isSubmitting}
               >
                 Cancella tutto

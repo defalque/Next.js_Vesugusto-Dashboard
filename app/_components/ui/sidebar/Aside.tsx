6848f1dfd06cb249a4d9230df3ccc9a0cbc4f659
Marco <marcodefalco2017@libero.it>
2025-12-17 09:18:37 +0100

    sidebar height bug: from screen to svh

--- app/_components/ui/sidebar/Aside.tsx
+++ app/_components/ui/sidebar/Aside.tsx
@@ -7,7 +7,7 @@ function Aside({ children }: { children: React.ReactNode }) {
       layout
       layoutRoot
       layoutScroll
-      className="text-sidebar bg-box inset-y-0 left-0 z-50 row-span-full row-start-1 hidden h-screen w-60 items-center space-y-1 divide-y divide-gray-200 border-r border-gray-200 xl:flex xl:flex-col dark:divide-zinc-700/40 dark:border-zinc-700/40"
+      className="text-sidebar bg-box inset-y-0 left-0 z-50 row-span-full row-start-1 hidden h-svh w-60 items-center space-y-1 divide-y divide-gray-200 border-r border-gray-200 xl:flex xl:flex-col dark:divide-zinc-700/40 dark:border-zinc-700/40"
       style={{ position: "fixed" }}
     >
       {children}
